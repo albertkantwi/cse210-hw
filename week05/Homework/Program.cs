8ccfbfff005ef77a430722c8084a246ffbc791c6
Albert <albertkantwi@gmail.com>
2026-04-11 22:40:29 +0000

    Add home work assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment test = new Assignment("Albert Antwi", "How to take a test");
+
+        Console.WriteLine(test.GetSummary());
+
+        MathAssignment mathHomework = new MathAssignment("Albert", "Algebra 2", "6.3", "1-100");
+        Console.WriteLine(mathHomework.GetHomeworkList());
+
+        WritingAssignment paper = new WritingAssignment("Albert Antwi", "English 121", "10 Tips for Getting an A on that Test");
+        Console.WriteLine(paper.GetWritingInformation()); 
     }
 }
