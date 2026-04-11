17711e867a8355165c13cf6db90a323faf2d93f7
Albert <albertkantwi@gmail.com>
2026-04-11 22:54:02 +0000

    Add Mindfulness assignment

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -1,9 +1,15 @@
 using System;
 
 class Program
+
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
+
+
+        Menu.DisplayMenu(); 
+
     }
+        
 }
