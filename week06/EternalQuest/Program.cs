270444a51f781a5a1c891b830b4cc5b260f23883
Albert <albertkantwi@gmail.com>
2026-04-15 21:27:12 +0000

    Finish week6 assignment

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,15 @@
 using System;
 
 class Program
+
 {
+
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        Console.WriteLine("Welcome to EternalQuest where we help you achieve your goals!");
+        GoalManager.Start();
+
+
     }
 }
